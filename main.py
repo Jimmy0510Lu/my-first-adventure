c99d0381a118316e492adc90bfb1db92cb65b75b
Jimmy0510Lu <james.lu0510@gmail.com>
2026-02-07 05:21:45 +0800

    fix: resolve TypeError by adding type conversion

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # main.py
 def welcome():
     print("--- 歡迎來到冒險者計算機 ---")
-    name = input("請輸入你的冒險者稱號: ")
-    print(f"你好, {name}! 準備好進行第一次運算了嗎?")
+    #name = input("請輸入你的冒險者稱號: ")
+    #print(f"你好, {name}! 準備好進行第一次運算了嗎?")
+    num_str = "100"
+    result = int(num_str) / 5
+    print(f"計算結果: 100 除以 5 等於 {result}")
+    
     
 if __name__ == "__main__":
     welcome()
